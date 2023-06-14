8d52b3d975cb97814f2f956994f035c679f714cb
Jesse Snyder <jessetsnyder@gmail.com>
2023-06-14 15:06:55 -0500

    use RIA instead of AST

--- src/AstriaToken.sol
+++ src/AstriaToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin4.9.0/contracts/token/ERC20/ERC20.sol";
 
 contract AstriaToken is ERC20 {
-    constructor() ERC20("Astria", "AST") {
+    constructor() ERC20("Astria", "RIA") {
         _mint(msg.sender, 1000000000000000000000000000);
     }
 }
